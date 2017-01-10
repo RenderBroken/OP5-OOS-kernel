fe0672cc6a2e179df1da16d8382edc15176356d7
Oleg Nesterov <oleg@redhat.com>
2017-01-09 20:26:14 -0800

    Use after free from pid_nr_ns()
    There is use after free reported due to group
    leader task is already freed but other tasks are
    still holding the group leader task address in
    task->group_leader pointer.
    
    pid_nr_ns+0x10/0x38
    cgroup_pidlist_start+0x144/0x400
    cgroup_seqfile_start+0x1c/0x24
    kernfs_seq_start+0x54/0x90
    seq_read+0x15c/0x3a8
    kernfs_fop_read+0x38/0x160
    __vfs_read+0x28/0xc8
    vfs_read+0x84/0xfc
    
    Change-Id: Ib6b3fc75bf0d24a04455bf81d54900c21c434958
    Signed-off-by: Oleg Nesterov <oleg@redhat.com>
    Signed-off-by: Prasad Sodagudi <psodagud@codeaurora.org>
    Signed-off-by: Vaishali Gupta <Vaishali@codeaurora.org>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -2099,11 +2099,6 @@ static inline pid_t task_tgid_nr(struct task_struct *tsk)
 	return tsk->tgid;
 }
 
-static inline pid_t task_tgid_vnr(struct task_struct *tsk)
-{
-	return pid_vnr(task_tgid(tsk));
-}
-
 static inline int pid_alive(const struct task_struct *p);
 static inline pid_t task_tgid_nr_ns(struct task_struct *tsk, struct pid_namespace *ns);
 static inline pid_t task_ppid_nr_ns(const struct task_struct *tsk, struct pid_namespace *ns)
@@ -2149,6 +2144,11 @@ static inline pid_t task_session_vnr(struct task_struct *tsk)
 static inline pid_t task_tgid_nr_ns(struct task_struct *tsk, struct pid_namespace *ns)
 {
 	return __task_pid_nr_ns(tsk, __PIDTYPE_TGID, ns);
+}
+
+static inline pid_t task_tgid_vnr(struct task_struct *tsk)
+{
+	return __task_pid_nr_ns(tsk, __PIDTYPE_TGID, NULL);
 }
 
 /* obsolete, do not use */
