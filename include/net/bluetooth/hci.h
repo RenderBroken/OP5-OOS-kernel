a8b835b54fbd1c14e22f0c75c4dd5b1407bc46fb
RenderBroken <zkennedy87@gmail.com>
2017-08-05 13:20:05 -0400

    include/net/bluetooth/hci.h: Extend HCI_CMD_TIMEOUT to 8 seconds
    Signed-off-by: RenderBroken <zkennedy87@gmail.com>

--- include/net/bluetooth/hci.h
+++ include/net/bluetooth/hci.h
@@ -261,7 +261,7 @@ enum {
 #define HCI_DISCONN_TIMEOUT	msecs_to_jiffies(2000)	/* 2 seconds */
 #define HCI_PAIRING_TIMEOUT	msecs_to_jiffies(60000)	/* 60 seconds */
 #define HCI_INIT_TIMEOUT	msecs_to_jiffies(10000)	/* 10 seconds */
-#define HCI_CMD_TIMEOUT		msecs_to_jiffies(2000)	/* 2 seconds */
+#define HCI_CMD_TIMEOUT		msecs_to_jiffies(8000)	/* 8 seconds */
 #define HCI_ACL_TX_TIMEOUT	msecs_to_jiffies(45000)	/* 45 seconds */
 #define HCI_AUTO_OFF_TIMEOUT	msecs_to_jiffies(2000)	/* 2 seconds */
 #define HCI_POWER_OFF_TIMEOUT	msecs_to_jiffies(5000)	/* 5 seconds */
