b16cecaf9de842ed4f015771dd25bc4288deb819
Brendan Jackman <brendan.jackman@arm.com>
2017-08-07 12:31:27 +0100

    FROMLIST: sched/fair: Fix slow-path wakeup when not on sibling of prev_cpu
    The case in question is this:
    
     - select_task_rq_fair has been sent to the slow path by wake_wide or
       wake_cap.
     - prev_cpu != smp_processor_id()
     - smp_processor_id() is idle and all the other CPUs are busy
       according to the measures used in find_idlest_group.
    
    Then find_idlest_group will return NULL on each invocation,
    indicating that the group containing 'cpu' (which is currently set to
    smp_processor_id()) is the idlest. This means we'll exit the
    'while (sd)' loop without having changed new_cpu from its initial
    value, which is prev_cpu. So we'll return prev_cpu despite having
    just determined that smp_processor_id() is a better target.
    
    It looks like until wake_wide and wake_cap came into play, this
    wasn't a problem, because we only used find_idlest_group & co. when
    smp_processor_id() wasn't in p->cpus_allowed.
    
    So this patch sets new_cpu to 'cpu' (which, in the case in question,
    equals smp_processor_id()) when find_idlest_group returns NULL.
    
    Signed-off-by: Brendan Jackman <brendan.jackman@arm.com>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Cc: Josef Bacik <josef@toxicpanda.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Morten Rasmussen <morten.rasmussen@arm.com>
    Cc: Peter Zijlstra <peterz@infradead.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7243,6 +7243,7 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int sd_flag, int wake_f
 
 			group = find_idlest_group(sd, p, cpu, sd_flag);
 			if (!group) {
+                                new_cpu = cpu;
 				sd = sd->child;
 				continue;
 			}
